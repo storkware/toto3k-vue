8478e6c7468a779c83e97e8fe57699631c2eb777
Pierre Martin-Henry <pierre.martin@live.com>
2017-04-22 16:00:22 +0200

    Rebrand project

--- app/src/renderer/components/LandingPageView.vue
+++ app/src/renderer/components/LandingPageView.vue
@@ -1,30 +1,15 @@
 <template>
   <div>
-    <img src="./LandingPageView/assets/logo.png" alt="electron-vue">
-    <h1>Welcome.</h1>
-    <current-page></current-page>
-    <versions></versions>
-    <links></links>
+    <h1>Hello world!</h1>
   </div>
 </template>
 
 <script>
-  import CurrentPage from './LandingPageView/CurrentPage'
-  import Links from './LandingPageView/Links'
-  import Versions from './LandingPageView/Versions'
   export default {
-    components: {
-      CurrentPage,
-      Links,
-      Versions
-    },
+    components: {},
     name: 'landing-page'
   }
 </script>
 
 <style scoped>
-  img {
-    margin-top: -25px;
-    width: 450px;
-  }
 </style>
